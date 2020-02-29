a48e36e1681e14a79abca6e015a4b581384dc266
KriZz87 <32612882+ticzz@users.noreply.github.com>
2020-02-29 17:19:16 +0100

    Update FOVchanger.lua

--- FOVchanger.lua
+++ FOVchanger.lua
@@ -1,5 +1,6 @@
 local visuals_menu = gui.Reference("VISUALS", "OTHER", "Effects")
 local visuals_custom_viewmodel_editor = gui.Checkbox( visuals_menu, "lua_custom_viewmodel_editor", "Custom Viewmodel Editor", 0 );
+local S,e=client.SetConVar,gui.Checkbox(gui.Reference("VISUALS", "OTHER", "Effects"), 'lua_disable_panorama_blur', 'Disable Panorama Blur', false)
 local xO = client.GetConVar("viewmodel_offset_x");
 local yO = client.GetConVar("viewmodel_offset_y");
 local zO = client.GetConVar("viewmodel_offset_z");
@@ -9,10 +10,10 @@ local xS = gui.Slider(visuals_menu, "lua_x", "X", xO, -20, 20);
 local yS = gui.Slider(visuals_menu, "lua_y", "Y", yO, -100, 100); 
 local zS = gui.Slider(visuals_menu, "lua_z", "Z", zO, -20, 20); 
 local vfov = gui.Slider(visuals_menu, "vfov", "Viewmodel FOV", fO, 0, 120);
-local fov = gui.Slider(visuals_menu, "fov", "Viewmodel FOV", f1, 0, 120);
+local fov = gui.Slider(visuals_menu, "fov", "View FOV", f1, 0, 120);
+
 
 local function Visuals_Viewmodel()
-
    if visuals_custom_viewmodel_editor:GetValue() then
 client.SetConVar("viewmodel_offset_x", xS:GetValue(), true);
 client.SetConVar("viewmodel_offset_y", yS:GetValue(), true);
@@ -23,30 +24,15 @@ client.SetConVar("fov_cs_debug", fov:GetValue(), true);
    end
 
 
-
-local noshadow_ref = gui.Reference("Visuals", "Other", "Effects")
-local lua_noshadows = gui.Checkbox(noshadow_ref, "lua_noshadows", "No Shadows", false)
-
-function noshadows()
-    if lua_noshadows:GetValue() then
-        client.SetConVar("r_shadows", 0, true);
-        client.SetConVar("cl_csm_static_prop_shadows", 0, true );
-        client.SetConVar("cl_csm_shadows", 0, true );
-        client.SetConVar("cl_csm_world_shadows", 0, true );
-        client.SetConVar("cl_foot_contact_shadows", 0, true );
-        client.SetConVar("cl_csm_viewmodel_shadows", 0, true );
-        client.SetConVar("cl_csm_rope_shadows", 0, true );
-        client.SetConVar("cl_csm_sprite_shadows", 0, true );
-            else
-            client.SetConVar("r_shadows", 1, true);
-            client.SetConVar("cl_csm_static_prop_shadows", 1, true );
-            client.SetConVar("cl_csm_shadows", 1, true );
-            client.SetConVar("cl_csm_world_shadows", 1, true );
-            client.SetConVar("cl_foot_contact_shadows", 1, true );
-            client.SetConVar("cl_csm_viewmodel_shadows", 1, true );
-            client.SetConVar("cl_csm_rope_shadows", 1, true );
-            client.SetConVar("cl_csm_sprite_shadows", 1, true );
-    end
+local function noshadows()
+    client.SetConVar( "r_shadows", 0, true );
+    client.SetConVar( "cl_csm_static_prop_shadows", 0, true );
+    client.SetConVar( "cl_csm_shadows", 0, true );
+    client.SetConVar( "cl_csm_world_shadows", 0, true );
+    client.SetConVar( "cl_foot_contact_shadows", 0, true );
+    client.SetConVar( "cl_csm_viewmodel_shadows", 0, true );
+    client.SetConVar( "cl_csm_rope_shadows", 0, true );
+    client.SetConVar( "cl_csm_sprite_shadows", 0, true );
 end
 
 
@@ -60,5 +46,4 @@ noshadows()
 
 client.AllowListener("round_start")
 callbacks.Register ("FireGameEvent", event)
-callbacks.Register("Draw", "No Shadows", noshadows);
 callbacks.Register("Draw", "Custom Viewmodel Editor", Visuals_Viewmodel)
