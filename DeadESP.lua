9aa410b6765e30c44b73989becc212ec6c7d1d29
KriZz87 <32612882+ticzz@users.noreply.github.com>
2020-03-07 12:09:50 +0100

    Update DeadESP.lua

--- DeadESP.lua
+++ DeadESP.lua
@@ -1,48 +1,40 @@
 local toggle = true
 callbacks.Register("Draw", function()
-   local local_player = entities.GetLocalPlayer()
-   if (local_player == NULL or local_player == nil) then
-       return
-   else
-   if (input.IsButtonReleased(78)) then   	-- N key toggles DeadESP on/off at all. If u want another key change number. 
-						--(if u need help to find numbers of keys use: https://keycode.info/)
-           toggle = not toggle
-   end
-       if (local_player:IsAlive()) then
-	local w, h = draw.GetScreenSize()
-        draw.SetFont(draw.CreateFont("Tahoma", 25, 100))
-        draw.Color(128, 0, 0, 255)
-        draw.TextShadow(415, 1046, "On")
+    local local_player = entities.GetLocalPlayer()
+    if (local_player == nil) then
+        return
+    else
+        if (input.IsButtonReleased(78)) then		-- N key toggles DeadESP on/off at all. If u want another key change number. 
+							--(if u need help to find numbers of keys use: https://keycode.info/)
+            toggle = not toggle
         end
-           gui.SetValue("esp.chams.enemy.occluded", 0)
-	   gui.SetValue("esp.chams.enemy.overlay", 0)
-           gui.SetValue("esp.chams.enemy.visible", 4)
-           gui.SetValue("esp.overlay.enemy.health.healthbar", 0)
-           gui.SetValue("esp.overlay.enemy.weapon", 0)
-           gui.SetValue("esp.overlay.enemy.name", 0)
-           gui.SetValue("esp.overlay.enemy.box", 0)
-           gui.SetValue("esp.overlay.enemy.precision", 0)
-           gui.SetValue("esp.overlay.enemy.scoped", 0)
-           gui.SetValue("esp.overlay.enemy.planting", 0)
-           gui.SetValue("esp.overlay.enemy.flashed", 0)
-           gui.SetValue("esp.overlay.enemy.reloading", 0)
-           gui.SetValue("esp.overlay.enemy.defusing", 0)
-	   gui.SetValue("esp.overlay.weapon.ammo", 0)
-      else
-           gui.SetValue("esp.chams.enemy.occluded", 3)
-	   gui.SetValue("esp.chams.enemy.overlay", 0)
-           gui.SetValue("esp.chams.enemy.visible", 4)
-           gui.SetValue("esp.overlay.enemy.health.healthbar", 1)
-           gui.SetValue("esp.overlay.enemy.weapon", 1)
-           gui.SetValue("esp.overlay.enemy.name", 1)
-           gui.SetValue("esp.overlay.enemy.box", 1)
-           gui.SetValue("esp.overlay.enemy.precision", 1)
-	   gui.SetValue("esp.overlay.enemy.scoped", 1)
-           gui.SetValue("esp.overlay.enemy.planting", 1)
-           gui.SetValue("esp.overlay.enemy.flashed", 1)
-           gui.SetValue("esp.overlay.enemy.reloading", 1)
-           gui.SetValue("esp.overlay.enemy.defusing", 1)
-	   gui.SetValue("esp.overlay.weapon.ammo", 1)
-       end
-   end
+        if (local_player:IsAlive() and not toggle) then
+            gui.SetValue("esp.overlay.enemy.name", false)
+            gui.SetValue("esp.chams.enemy.occluded", false)
+            gui.SetValue("esp.overlay.enemy.scoped", false)
+            gui.SetValue("esp.overlay.enemy.reloading", false)
+            gui.SetValue("esp.overlay.enemy.health.healthnum", false)
+            gui.SetValue("esp.overlay.enemy.health", false)
+            gui.SetValue("esp.overlay.enemy.weapon", "0")
+            gui.SetValue("esp.overlay.enemy.box", "0")
+            gui.SetValue("esp.overlay.enemy.hasdefuser", "0")
+            gui.SetValue("esp.overlay.enemy.hasc4", "0")
+        else
+            if (local_player:IsAlive()) then
+                local w, h = draw.GetScreenSize()
+                draw.SetFont(draw.CreateFont("Tahoma", 25, 100))
+                draw.Color(128, 0, 0, 255)
+                draw.TextShadow(335, 1045, "Visuals On")
+            end
+            gui.SetValue("esp.overlay.enemy.name", true)
+            gui.SetValue("esp.chams.enemy.occluded", 3)
+            gui.SetValue("esp.overlay.enemy.scoped", 1)
+            gui.SetValue("esp.overlay.enemy.reloading", 1)
+            gui.SetValue("esp.overlay.enemy.health.healthnum", 1)
+            gui.SetValue("esp.overlay.enemy.weapon", "1")
+            gui.SetValue("esp.overlay.enemy.box", "1")
+            gui.SetValue("esp.overlay.enemy.hasdefuser", "1")
+            gui.SetValue("esp.overlay.enemy.hasc4", "1")
+        end
+    end
 end)
