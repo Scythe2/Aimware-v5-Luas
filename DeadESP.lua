964cc49caac06780fa40a77856f3cda5376aac43
KriZz87 <32612882+ticzz@users.noreply.github.com>
2020-03-08 15:52:09 +0100

    Add files via upload

--- DeadESP.lua
+++ DeadESP.lua
@@ -1,40 +1,56 @@
-local toggle = true
-callbacks.Register("Draw", function()
-    local local_player = entities.GetLocalPlayer()
-    if (local_player == nil) then
-        return
-    else
-        if (input.IsButtonReleased(78)) then		-- N key toggles DeadESP on/off at all. If u want another key change number. 
-							--(if u need help to find numbers of keys use: https://keycode.info/)
-            toggle = not toggle
-        end
-        if (local_player:IsAlive() and not toggle) then
-            gui.SetValue("esp.overlay.enemy.name", false)
+-- DeadESP
+local w,h = 0,0
+local rage_ref_extra = gui.Reference("RAGEBOT", "AIMBOT", "AUTOMATION");
+local changeKey = gui.Keybox(rage_ref_extra, "wallhackkey", "Wallhack key", 18); 
+
+
+local function ESP_Always_OnDead( ) 
+
+w, h = draw.GetScreenSize()
+
+   if entities.GetLocalPlayer() == NULL then
+  return
+	end
+
+
+	if entities.GetLocalPlayer():IsAlive() == true and (input.IsButtonDown(changeKey:GetValue())) then			
+			
+			draw.Color(128,0,0,255)
+			draw.TextShadow(w/2,h/2, "Visuals On")
+			gui.SetValue("esp.chams.enemy.occluded","4")		
+
+	else
+		if entities.GetLocalPlayer():IsAlive() == true then
+            
+			gui.SetValue("esp.overlay.enemy.name", false)
             gui.SetValue("esp.chams.enemy.occluded", false)
             gui.SetValue("esp.overlay.enemy.scoped", false)
             gui.SetValue("esp.overlay.enemy.reloading", false)
             gui.SetValue("esp.overlay.enemy.health.healthnum", false)
-            gui.SetValue("esp.overlay.enemy.health", false)
-            gui.SetValue("esp.overlay.enemy.weapon", "0")
-            gui.SetValue("esp.overlay.enemy.box", "0")
-            gui.SetValue("esp.overlay.enemy.hasdefuser", "0")
-            gui.SetValue("esp.overlay.enemy.hasc4", "0")
-        else
-            if (local_player:IsAlive()) then
-                local w, h = draw.GetScreenSize()
-                draw.SetFont(draw.CreateFont("Tahoma", 25, 100))
-                draw.Color(128, 0, 0, 255)
-                draw.TextShadow(335, 1045, "Visuals On")
-            end
+            gui.SetValue("esp.overlay.enemy.health.healthbar", false)
+            gui.SetValue("esp.overlay.enemy.weapon", false)
+            gui.SetValue("esp.overlay.enemy.box", false)
+            gui.SetValue("esp.overlay.enemy.hasdefuser", false)
+            gui.SetValue("esp.overlay.enemy.hasc4", false)
+			gui.SetValue("esp.chams.enemy.overlay", false)
+			gui.SetValue("esp.overlay.weapon.ammo", false)
+
+	elseif entities.GetLocalPlayer():IsAlive() == false then
+
             gui.SetValue("esp.overlay.enemy.name", true)
-            gui.SetValue("esp.chams.enemy.occluded", 3)
-            gui.SetValue("esp.overlay.enemy.scoped", 1)
-            gui.SetValue("esp.overlay.enemy.reloading", 1)
-            gui.SetValue("esp.overlay.enemy.health.healthnum", 1)
-            gui.SetValue("esp.overlay.enemy.weapon", "1")
-            gui.SetValue("esp.overlay.enemy.box", "1")
-            gui.SetValue("esp.overlay.enemy.hasdefuser", "1")
-            gui.SetValue("esp.overlay.enemy.hasc4", "1")
-        end
-    end
-end)
+            gui.SetValue("esp.chams.enemy.occluded", "4")
+            gui.SetValue("esp.overlay.enemy.scoped", true)
+            gui.SetValue("esp.overlay.enemy.reloading", true)
+            gui.SetValue("esp.overlay.enemy.health.healthnum", true)
+            gui.SetValue("esp.overlay.enemy.health.healthbar", true)
+			gui.SetValue("esp.overlay.enemy.weapon", true)
+            gui.SetValue("esp.overlay.enemy.box", true)
+            gui.SetValue("esp.overlay.enemy.hasdefuser", true)
+            gui.SetValue("esp.overlay.enemy.hasc4", true)
+			gui.SetValue("esp.chams.enemy.overlay", Wireframe)
+end
+end			
+end	
+callbacks.Register( "Draw", "espalwaysondead", ESP_Always_OnDead );
+
+-- End DeadESP
